614478599bcd4f7f01f7792f15896891cca65dad
stm <no-reply@example.com>
2025-01-27 22:12:49 -0500

    [monotonic stack] 85 is a 2D version of 84, 316 requires a set to track seen

--- leetcodePython/Stack/stack_316_1081.py
+++ leetcodePython/Stack/stack_316_1081.py
@@ -4,12 +4,15 @@ class Solution316:
         seen = set()
         lastIndexOfCh = {}
 
+        # store the last index of each character s = "bcabc"
         for i, c in enumerate(s):
-            lastIndexOfCh[c] = i
+            lastIndexOfCh[c] = i # {'b': 3, 'c': 4, 'a': 2}
 
         for i in range(len(s)):
+            # using a set to track visited characters is unique.
             if s[i] not in seen:
-                while increaseChars and s[i] < increaseChars[-1] and i < lastIndexOfCh[increaseChars[-1]]:
+                # if the current character is smaller than the last character in the stack
+                while increaseChars and s[i] < increaseChars[-1] and i < lastIndexOfCh[increaseChars[-1]]: #
                     seen.discard(increaseChars.pop())
                 seen.add(s[i])
                 increaseChars.append(s[i])
--- leetcodePython/Stack/stack_85.py
+++ leetcodePython/Stack/stack_85.py
@@ -4,18 +4,24 @@ from typing import List
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         maxArea = 0
+         # historgram for the current row
         histogram = [0]*len(matrix[0])
+        # for each row, update the histogram and calculate the max area
         for row in range(len(matrix)):
+            # update the histogram for the current row
             self._updateHistogramForRow(row, matrix, histogram)
+            # calculate the max area for the current histogram
             currMaxArea = self._largestHistogramRectangle(histogram)
             maxArea = max(maxArea, currMaxArea)
         return maxArea
 
     @staticmethod
     def _updateHistogramForRow(row: int, matrix: List[List[str]], histogram: List[int]) -> None:
+        # update the histogram for the current row
         for col in range(len(matrix[0])):
+            # if the current element is '1', increment the histogram value
             if matrix[row][col] == '1':
-                histogram[col] = histogram[col] + 1
+                histogram[col] = histogram[col] + 1 # [1, 0, 1, 0, 0]
             else:
                 histogram[col] = 0
 
@@ -24,16 +30,21 @@ class Solution:
         increaseHeightIndex = [-1]
         maxArea = 0
         heights.append(0)
+        # for each height on this row of histogram, calculate the max area
         for i in range(len(heights)):
             newHeight = heights[i]
+            # if the current height is smaller than the previous height
             while increaseHeightIndex[-1] != -1:
                 maxHeight = heights[increaseHeightIndex[-1]]
                 if newHeight > maxHeight:
                     break
                 increaseHeightIndex.pop()
+                # calculate the area
                 smallerHeightIndex = increaseHeightIndex[-1]
                 width = i - smallerHeightIndex - 1
                 maxArea = max(maxArea, maxHeight*width)
+            # add the current height to the stack
             increaseHeightIndex.append(i)
+        # remove the last element
         heights.pop()
         return maxArea
