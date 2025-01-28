fac3a296c1a3a734600755cde0d5908c0b7c0607
stm <no-reply@example.com>
2025-01-27 22:38:28 -0500

    [monotonic stack] cleaner solution.

--- leetcodePython/Stack/stack_402.py
+++ leetcodePython/Stack/stack_402.py
@@ -13,3 +13,26 @@ class Solution:
 
         numStr = "".join(remainDigits).lstrip('0')
         return numStr if numStr else "0"
+
+
+#------------------------------------------------- Cleaner solution
+    def removeKdigits2(self, num: str, k: int) -> str:
+        # num = "1432219", k = 3
+        # keep lexicographically order similar to 316
+        increStk = []
+        # greedy works 
+
+        for curr in range(len(num)):
+            # increStk [1, 4], curr 2, num[curr] 3
+            # len(num) 7, curr 2, len(increStk) 2 : 3 > 3
+            while increStk and num[curr] < increStk[-1] and k > 0:
+                increStk.pop()
+                k-=1
+            increStk.append(num[curr])
+
+        if k > 0:
+            increStk = increStk[:-k]
+
+
+        res = ''.join(increStk).lstrip('0')
+        return res if res else '0'
