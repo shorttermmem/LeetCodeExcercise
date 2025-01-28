b8d71052c849ae9eac2f8a22aeea57f1ad44ac94
stm <no-reply@example.com>
2025-01-27 22:56:36 -0500

    [monotonic stack] easy problem without using index.

--- leetcodePython/Stack/stack_496.py
+++ leetcodePython/Stack/stack_496.py
@@ -5,6 +5,8 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         greaterMap = {}
         decreaseStack = []
+        
+        # first exmaple that we dont need to use index of the array
         for num in nums2:
             while decreaseStack and num > decreaseStack[-1]:
                 prevNum = decreaseStack.pop()
@@ -12,9 +14,41 @@ class Solution:
             decreaseStack.append(num)
         answer = []
 
+        # use greaterMap to get the answer, good name for clarity
         for num in nums1:
             if num in greaterMap:
                 answer.append(greaterMap[num])
             else:
                 answer.append(-1)
         return answer
+
+
+#-------------------------------------------------- with example
+    def nextGreaterElement2(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+        # Output: [-1,3,-1]
+
+        # no bigger to the right of 4, -1
+        # bigger to the right of 1, 3
+        # no bigger to the right of 2, -1
+
+        decreStk = []
+        greaterMap = {}
+        res = []
+
+        for n in nums2:
+            
+            # bigger than top of stk
+            while decreStk and decreStk[-1] < n: # [3, ]
+                prevNum = decreStk.pop()
+                greaterMap[prevNum] = n
+            decreStk.append(n)                   # [3, ]
+
+        for n in nums1:
+            if n in greaterMap:
+                res.append(greaterMap[n])
+            else:
+                res.append(-1)
+
+        return res
+        
