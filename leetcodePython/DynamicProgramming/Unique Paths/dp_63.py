b70c1a1dcf626cbef4cd62a49b6e5e048b871116
stm <no-reply@github.com>
2025-02-24 20:47:08 -0500

    [dp] solve two more dp problems

--- leetcodePython/DynamicProgramming/Unique Paths/dp_63.py
+++ leetcodePython/DynamicProgramming/Unique Paths/dp_63.py
@@ -9,3 +9,47 @@
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        
+        # states
+        # ways_to_curr[i][j]
+
+        # actions
+        # ways_to_curr[i][j] = ways_to_curr[i-1][j] + ways_to_curr[i][j-1]
+
+        # transitions
+        # ways_to_curr[i][j] = 0 if obstacleGrid[i][j] else (ways_to_curr[i-1][j] + ways_to_curr[i][j-1])
+        def tabulation():
+            h, w = len(obstacleGrid), len(obstacleGrid[0])
+            ways_to_curr = [[0] * (w+1)] * (h + 1)
+
+            ways_to_curr[1][1] = 1
+            for i in range(1, h+1):
+                for j in range(1, w+1):
+                    ways_to_curr[i][j] = 0 if obstacleGrid[i - 1][j - 1] == 1 else (ways_to_curr[i-1][j] + ways_to_curr[i][j-1])
+            
+            return ways_to_curr[-1][-1]
+
+        # referencing max 1, requires only two variables, left and top
+        def tabulation_with_memory_opt():
+            h, w = len(obstacleGrid) + 1, len(obstacleGrid[0]) + 1
+
+            #       | top[0 .. w]
+            #  left | ways_to_curr
+            ways_to_curr = 1
+            left = 0
+            top = [0] * (w)
+            
+            top[1] = 1
+            for i in range(1, h):
+                for j in range(1, w):
+                    if j == 1:
+                        left = 0
+
+                    if obstacleGrid[i - 1][j - 1] == 1:
+                        left, top[j] = 0, 0
+                              
+                    ways_to_curr = left + top[j]                    
+                    left = ways_to_curr
+                    top[j] = ways_to_curr    
+
+            return ways_to_curr
