3cd2e3ab45b7cf8cbf308589d5e9e5fba2ef450b
stm <803754+shorttermmem@users.noreply.github.com>
2025-08-01 13:07:53 -0400

    [mock] 20250802 - init

---
+++ MockPython/20250802/Minimum Insertion Steps to Make a String Palindrome.py
@@ -0,0 +1,43 @@
+#https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+from common_types import *
+"""
+1312. Minimum Insertion Steps to Make a String Palindrome
+Hard
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s. In one step you can insert any character at any index of the string.
+
+Return the minimum number of steps to make s palindrome.
+
+A Palindrome String is one that reads the same backward as well as forward.
+
+ 
+
+Example 1:
+
+Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we do not need any insertions.
+Example 2:
+
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".
+Example 3:
+
+Input: s = "leetcode"
+Output: 5
+Explanation: Inserting 5 characters the string becomes "leetcodocteel".
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+"""
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        return 0
