83cde17bb059070e887db3b02d5a531e2b0bb198
stm <803754+shorttermmem@users.noreply.github.com>
2025-08-02 12:35:52 -0400

    update common header to include heapq.

--- MockPython/common_types.py
+++ MockPython/common_types.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable, Dict, List, Optional, Union
 from collections import defaultdict, deque, Counter, OrderedDict
 from itertools import combinations, permutations, product
+from heapq import *
 
 # Definition for singly-linked list.
 class ListNode:
