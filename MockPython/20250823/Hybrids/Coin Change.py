419c872c03c4016f88ec47c20358be14d61e04a7
stm <803754+shorttermmem@users.noreply.github.com>
2025-09-09 18:40:42 -0400

    [mock][DP] 20250830 - 3rd batch
    Co-authored-by: Jango <JangoBoogaloo@users.noreply.github.com>

--- MockPython/20250823/Hybrids/Coin Change.py
+++ MockPython/20250823/Hybrids/Coin Change.py
@@ -4,11 +4,90 @@ from common_types import *
 """
 You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 
-Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+Return the fewest number of coins that you need to make up that amount. 
+If that amount of money cannot be made up by any combination of the coins, return -1.
 
 You may assume that you have an infinite number of each kind of coin.
+
+
+Input: coins = [1,2,5], amount = 11
+
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+
+
+Input: coins = [2], amount = 3
+Output: -1
+
+coins: [y1, y2, y3]
+
+minCoinsToValue = [float('inf")] * (amount + 1)
+
+minCoinsToValue[0] = 0
+
+if amount == 0:
+    return 0
+
+take min:
+
+100000
+
+[100]
+
+1 -> 100000
+
+for x in range(amount+1):
+if x >= y1
+    minCoinsToValue[x] = min(minCoinsToValue[x], minCoinsToValue[x-y1] + 1)
+if x >= y2
+    minCoinsToValue[x] = min(minCoinsToValue[x], minCoinsToValue[x-y2] + 1)
+if x >= y3
+    minCoinsToValue[x] = min(minCoinsToValue[x], minCoinsToValue[x-y3] + 1)
+
+if minCoinsToValue[-1] == float('inf')
+    return -1
+
+return minCoinsToValue[-1]
+
 """
+from typing import List
+
 
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        return 0
+    def coinChange(self, coins: List[int], amount: int) -> float:
+        if amount == 0:
+            return 0
+        minCoinsToValue = [float('inf')] * (amount + 1)
+        minCoinsToValue[0] = 0
+        for currValue in range(1, amount + 1):
+            for coinValue in coins:
+                if currValue < coinValue:
+                    continue
+                minCoinsToValue[currValue] = min(minCoinsToValue[currValue], minCoinsToValue[currValue - coinValue] + 1)
+        if minCoinsToValue[-1] == float('inf'):
+            return -1
+        return minCoinsToValue[-1]
+
+
+
+class SolutionTopDown:
+    def _minCoinsToValue(self, coins, amount, minCoinsToValue) -> float:
+        if amount == 0:
+            return 0
+        if amount < 0:
+            return float('inf')
+        if minCoinsToValue[amount] != -1:
+            return minCoinsToValue[amount]
+        currMinCoins = float('inf')
+        for coinValue in coins:
+            currMinCoins = min(currMinCoins, self._minCoinsToValue(coins, amount - coinValue, minCoinsToValue) + 1)
+        minCoinsToValue[amount] = currMinCoins
+        return minCoinsToValue[amount]
+
+    def coinChange(self, coins: List[int], amount: int) -> float:
+        minCoinsToValue = [-1] * (amount + 1)
+        minCoins = self._minCoinsToValue(coins, amount, minCoinsToValue)
+        if minCoins == float('inf'):
+            return -1
+        return minCoins
--- MockPython/20250823/Hybrids/Longest Increasing Subsequence.py
+++ MockPython/20250823/Hybrids/Longest Increasing Subsequence.py
@@ -3,8 +3,75 @@ from common_types import *
 
 """
 Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+
+for i in range(len(nums)):
+    if nums[i] > nums[i-1]:
+        maxLengthEndAt[i] = maxLengthEndAt[i-1] + 1
+
+maxLengthEndAt[x]
+
+if nums[x] > nums[x-1]:
+    maxLengthEndAt[x] = maxLengthEndAt[x-1] + 1
+
+if nums[x] > nums[x-2]:
+    maxLengthEndAt[x] = maxLengthEndAt[x-2] + 1
+
+if nums[x] > nums[x-t]:
+    maxLengthEndAt[x] = maxLengthEndAt[x-t] + 1
+
+
+[......end]........[x]
+
+maxLength = 0
+
+
+if nums[?] > nums[]
+
+increasing:
+# nums[i-1] < nums[i]
+
+LIS(0, len(n)-1) = max(LIS(1, len(n)-1), LIS(0, len(n)-2))
+
+
+12341
+
 """
+from typing import List
 
 class Solution:
+    def _getMaxLengthEndAt(self, index, nums) -> int:
+        if index == 0:
+            return 1
+        maxLength = 1
+        for prevEnd in range(index-1, -1, -1):
+            if nums[index] > nums[prevEnd]:
+                # when a data is increasing, include it in a comparison
+                maxLength = max(maxLength, 1 + self._getMaxLengthEndAt(prevEnd, nums))
+        return maxLength
+
     def lengthOfLIS(self, nums: List[int]) -> int:
-        return 0
+        maxLength = self._getMaxLengthEndAt(len(nums)-1, nums)
+        return maxLength
+    
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+                  
+        n = len(nums)
+        dp = [1] * n
+        
+        for i in range(n):    
+            for j in range(0,i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+        return max(dp)    
