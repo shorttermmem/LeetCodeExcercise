58a962aa909a6c619f67f5a08a43c57a86887f83
stm <803754+shorttermmem@users.noreply.github.com>
2025-10-18 13:04:34 -0400

    [mock] 20251018 - 1st attempt Co-authored-by: Jango <JangoBoogaloo@users.noreply.github.com>

--- MockPython/20251018/Choose K Elements With Maximum Sum.py
+++ MockPython/20251018/Choose K Elements With Maximum Sum.py
@@ -7,11 +7,16 @@ You are given two integer arrays, nums1 and nums2, both of length n, along with 
 
 For each index i from 0 to n - 1, perform the following:
 
-Find all indices j where nums1[j] is less than nums1[i].
-Choose at most k values of nums2[j] at these indices to maximize the total sum.
+* Find all indices j where nums1[j] is less than nums1[i].
+* Choose at most k values of nums2[j] at these indices to maximize the total sum.
 Return an array answer of size n, where answer[i] represents the result for the corresponding index i.
 
+for each i:
+* find all j: nums1[j] < nums1[i]
 
+within j:
+k of nums2[j]
+max(sum(nums2[ k of j]))
 
 Example 1:
 
@@ -26,6 +31,32 @@ For i = 1: Select the 2 largest values from nums2 at index [2] where nums1[j] < 
 For i = 2: No indices satisfy nums1[j] < nums1[2], resulting in 0.
 For i = 3: Select the 2 largest values from nums2 at indices [0, 1, 2, 4] where nums1[j] < nums1[3], resulting in 50 + 30 = 80.
 For i = 4: Select the 2 largest values from nums2 at indices [1, 2] where nums1[j] < nums1[4], resulting in 30 + 20 = 50.
+
+
+[4,2,1,5,3]    [10,20,30,40,50]
+2,1,3 < 4
+[1,2,4]
+
+two data smaller than 3
+      2
+1, 2, 3, 4, 5
+2, 1, 4, 0, 3
+
+
+[50, 20, 30] => [20, 30, 50]
+k = 2 => 30 + 50 = 80
+O(n^2*logn)
+O(n*m*log(m) + n*n))
+
+* n indices
+* nlogn sorting
+
+
+sorting nums2 first, but build index relationship
+nums1 => index
+index => num2Value => sortedNum2Index
+
+
 Example 2:
 
 Input: nums1 = [2,2,2,2], nums2 = [3,1,2,3], k = 1
@@ -39,5 +70,32 @@ Since all elements in nums1 are equal, no indices satisfy the condition nums1[j]
 
 class Solution:
     def findMaxSum(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
-        return []
+        answer = []
+        for i, num in enumerate(nums1):
+            indicesFor_i = []
+            for i1 in range(len(nums1)):
+                if nums1[i1] < num:
+                    indicesFor_i.append(i1)
+            filteredNums2 = [nums2[j] for j in indicesFor_i]
+            filteredNums2.sort()
+            sum_i = 0
+            k2 = min(len(filteredNums2), k)
+            for t in range(k2):
+                sum_i += filteredNums2[len(filteredNums2)-1-t]
+            answer.append(sum_i)
+        return answer
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
     
--- MockPython/20251018/Shortest Path with Alternating Colors.py
+++ MockPython/20251018/Shortest Path with Alternating Colors.py
@@ -3,13 +3,23 @@
 from common_types import *
 
 """
-You are given an integer n, the number of nodes in a directed graph where the nodes are labeled from 0 to n - 1. Each edge is red or blue in this graph, and there could be self-edges and parallel edges.
+You are given an integer n, the number of nodes in a directed graph where the nodes are labeled from 0 to n - 1. 
+Each edge is red or blue in this graph, and there could be self-edges and parallel edges.
 
 You are given two arrays redEdges and blueEdges where:
 
-redEdges[i] = [ai, bi] indicates that there is a directed red edge from node ai to node bi in the graph, and
-blueEdges[j] = [uj, vj] indicates that there is a directed blue edge from node uj to node vj in the graph.
-Return an array answer of length n, where each answer[x] is the length of the shortest path from node 0 to node x such that the edge colors alternate along the path, or -1 if such a path does not exist.
+* redEdges[i] = [ai, bi] indicates that there is a directed red edge from node ai to node bi in the graph, and
+
+ai -> bi
+
+* blueEdges[j] = [uj, vj] indicates that there is a directed blue edge from node uj to node vj in the graph.
+
+uj => vj
+
+Return an array answer of length n, 
+where each answer[x] is the 
+length of the shortest path from node 0 to node x 
+such that the edge colors alternate along the path, or -1 if such a path does not exist.
 
  
 
@@ -17,11 +27,17 @@ Example 1:
 
 Input: n = 3, redEdges = [[0,1],[1,2]], blueEdges = []
 Output: [0,1,-1]
+
+0->1->2
+
+
 Example 2:
 
 Input: n = 3, redEdges = [[0,1]], blueEdges = [[2,1]]
 Output: [0,1,-1]
  
+   
+0->1<=2
 
 Constraints:
 
@@ -29,9 +45,118 @@ Constraints:
 0 <= redEdges.length, blueEdges.length <= 400
 redEdges[i].length == blueEdges[j].length == 2
 0 <= ai, bi, uj, vj < n
+
+
+redPath[src1].append(dst1)
+
+bluePath[src2].append(dst2)
+
+deque[0]
+
+for node in (n):
+    deque = [0]
+    path = 0
+    while deque:
+        for dst in deque:
+            # when reached node, quit
+
+        # when not reached node
+        path += 1
+
 """
 
 
 class Solution:
+    _RED = 0
+    _BLUE = 1
+    def _buildPath(self, edges: List[List[int]]):
+        path = defaultdict(list)
+        for src, dst in edges:
+            path[src].append(dst)
+        return path
+
+    def _getShortestPath(self, target, levelQue, redPath, bluePath) -> int:
+        path = 1
+        visited = set()
+        while levelQue:
+            levelSize = len(levelQue)
+            for _ in range(levelSize):
+                src, color = levelQue.popleft()
+                if (src, color) in visited:
+                    return -1
+                visited.add((src, color))
+                if src == target:
+                    return path
+                if color == self._RED:
+                    for dst in redPath[src]:
+                        levelQue.append((dst, self._BLUE))
+                else:
+                    for dst in bluePath[src]:
+                        levelQue.append((dst, self._RED))
+            path += 1
+        return -1
+
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        return []
+        redPath = self._buildPath(redEdges)
+        bluePath = self._buildPath(blueEdges)
+        levelQue = deque()
+        answer = [0]
+        for target in range(1, n):
+            if target in answer:
+                continue
+            for dst in redEdges[0]:
+                levelQue.append((dst, self._BLUE))
+            for dst in bluePath[0]:
+                levelQue.append((dst, self._RED))
+            path = self._getShortestPath(target, levelQue, redPath, bluePath)
+            levelQue.clear()
+            answer.append(path)
+        
+        return answer
+    
+
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        # create two seperate tables for each color and map all the connected edges.
+        dRed=defaultdict(list)
+        dBlue=defaultdict(list)
+
+        for a, b in redEdges:
+            dRed[a].append(b)
+        
+        for a, b in blueEdges:
+            dBlue[a].append(b)
+
+        #create list of size n with default value equals -1.
+
+        ans=[-1]*n
+        # we will be using heap as ques ask for shortest path.
+
+        heap=[]
+        
+        #initially we will push 3 elements in heap (count, node, color).
+        #default count is zero, default node is 0 and default color is None.
+        heapq.heappush(heap, [0,0,None])
+
+        # create a set called visited just to avoid cycle of coming to same node again and again.
+        visited=set()
+        
+        #rest is just a ideal BFS.
+        #if color is none, we can all "RED" and "BLUE" path both to the heap otherwise if color is "RED" then add "BLUE" and vice versa.
+        while heap:
+            cnt, node, color = heapq.heappop(heap)
+            if (node, color) not in visited:
+                if ans[node]==-1 or ans[node]>cnt:
+                    ans[node]=cnt
+                visited.add((node, color))
+                if color==None:
+                    for child in dRed[node]:
+                        heapq.heappush(heap, [cnt+1,child,"RED"])
+                    for child in dBlue[node]:
+                        heapq.heappush(heap, [cnt+1,child,"BLUE"])
+                elif color == "BLUE":
+                    for child in dRed[node]:
+                        heapq.heappush(heap, [cnt+1,child,"RED"])
+                elif color == "RED":
+                    for child in dBlue[node]:
+                        heapq.heappush(heap, [cnt+1,child,"BLUE"])
+        return ans
